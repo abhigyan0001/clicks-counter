f23051b8242abac7d846090c54bb40cf62439a59
tsdon0001 <mchatterjee6291@gmail.com>
2021-11-18 12:20:25 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [time, setTime] = useState(30);
+  const [clicks, setclicks] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(time-1);
+    }, 1000)
+    document.title = `${time<=0?"0":time}s left!`;
+    return () => clearInterval(timer);
+  }, [time, setTime]);
+
+  function incremeantClicks(){
+    setclicks(clicks+1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button style={{ height:"7em", width: "8em" }} onClick={incremeantClicks} disabled={time<=0?true:false}>Click here {time<=0?"[Disabled]":""}</button>
+    <h2>Clicks:{clicks}</h2>
+    <h2>Time left : {time<=0?"0":time}s</h2>
+    <h2>your cps: {time>0?`Waiting for ${time<=0?"0":time}s`:clicks/30+"cps"}</h2>
+    </>
   );
 }
 
